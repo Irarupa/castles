bf0fe64319e09f99d7d93045781a013f4e0a9e5e
Irarupa <62602926+Irarupa@users.noreply.github.com>
2020-05-02 14:12:03 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
@@ -12,32 +11,31 @@ function setup(){
     var canvas = createCanvas(1400,400);
     engine = Engine.create();
     world = engine.world;
-
+    ground = new Ground(300,400,2800,20);
     box1 = new Box(290,300,50,250);
-    box2 =new Box(340,300,50,200);
-    rect3 = new Box(390,300,50,150);
-    rect4 = new Box(440,300,50,200);
-    rect5 = new Box(496,300,50,200);
-    rect6 = new Box(550,300,82,200);
-    rect7 = new Box(240,300,20,200);
+    box2 =new Box(340,300,50,500);
+    rect3 = new Box(390,300,50,350);
+    rect4 = new Box(440,300,50,500);
+    rect5 = new Box(496,300,50,470);
+    rect6 = new Box(550,300,50,400);
+    rect7 = new Box(240,300,50,400);
      
-   rect8 = new Triangle (290,200,50,20);
-
-   rect9 = new Triangle(340,200,50,20);
-   rect10 = new Triangle(390,200,50,20);
-    rect11 = new Triangle(440,198,40,20);
-    rect12 = new Triangle (496,200,50,20);
-   rect13 = new Triangle(550,200,82,10);
-    rect14 = new Triangle(240,200,20,10)
-    ground = new Ground(300,400,1400,20);
+  rect8 = new Triangle (290,160,50,20);
+   rect9 = new Triangle(340,35,50,20);
+   rect10 = new Triangle(390,110,50,20);
+    rect11 = new Triangle(440,35,50,20);
+    rect12 = new Triangle (496,50,50,20);
+   rect13 = new Triangle(550,85,50,20);
+    rect14 = new Triangle(240,85,50,20)
+   
   
 }
 function draw(){
     background(0);
     Engine.update(engine);
+    ground.display();
     box1.display();
     box2.display();
-    ground.display();
     rect3.display();
     rect4.display();
     rect5.display();
@@ -54,3 +52,4 @@ function draw(){
     rect14.display();
     
 }
+
